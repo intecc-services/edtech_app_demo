64d184141f2a39b08edb817ba211c19a49dd2799
dani8h <mohammedanasdanish41@gmail.com>
2022-10-28 00:07:46 +0530

    initializing project

---
+++ android/app/src/main/kotlin/com/example/edtech_app_demo/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.edtech_app_demo
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(edtech_app());
+}
+
+class edtech_app extends StatelessWidget {
+  const edtech_app({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
